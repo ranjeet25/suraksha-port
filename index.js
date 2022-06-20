24dc33e607ebde687c39afd8ad4b8258b62814ac
RANJEET SAW <79698226+ranjeet25@users.noreply.github.com>
2022-06-21 02:11:28 +0530

    updates v 0.1

---
+++ index.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFun() {
+    var x = document.getElementById("myDV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
